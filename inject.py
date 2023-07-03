1a8aef5991afefb8892c14c1653ea45a738ac122
amrutaGN <95672779+amrutaGN@users.noreply.github.com>
2023-07-03 11:05:05 +0530

    Add files via upload

--- inject.py
+++ inject.py
@@ -1,60 +1,69 @@
 import sys
 import os
 from pathlib import Path
-import json 
-import random
+import json
+
+
+def find_parent_span(span, spans):
+    parent_span_id = None
+    for reference in span.get('references', []):
+        if reference.get('refType') == 'CHILD_OF':
+            parent_span_id = reference.get('spanID')
+            break
+    if parent_span_id:
+        parent_span = next((s for s in spans if s['spanID'] == parent_span_id), None)
+        return parent_span
+    return None
+
+
+def climb_up_spans(span, spans):
+    path = []
+    while span:
+        path.append(span)
+        span = find_parent_span(span, spans)
+    return path
 
 os.makedirs('../injected/', exist_ok=True)
 
 args = sys.argv
 
 fileOrFolder = args[1]
-min_err_prcnt = int(args[2])
-max_err_prcnt = int(args[3])
 
+call_graph = ["Service19", "Service7", "Service14","Service17"]  # Predefined list of service names
 
 files = []
 if os.path.isfile(fileOrFolder):
     files = [fileOrFolder]
-
 elif os.path.isdir(fileOrFolder):
     files = Path(fileOrFolder).glob('*.json')
 
 for file in files:
-    err_prcnt = random.randrange(min_err_prcnt, max_err_prcnt+1)
     data = json.load(open(file))
     fileName = os.path.split(file)[1]
 
-    # get specific tag
-    # check if there is error already
-    # inject error accordingly
-    for span in data['data'][0]['spans']:
-        # check if there are tags
-        if span.get('tags') is not None:
-            # check if there are error tags
-            for tag in span.get('tags'):
-                # has error = true
-                if tag.get('key') == 'error' and tag.get('type') == 'bool' and tag.get('value') == 'true':
-                    continue
-                else:
-                    if random.random() < err_prcnt / 100:
-                        span['tags'].append(
-                            [
-                                {
-                                    "key": "error",
-                                    "type": "bool",
-                                    "value": "true"
-                                }
-                            ]
-                        )
-        else:
-            if random.random() < err_prcnt / 100:
-                    span['tags'] = [
-                        {
-                            "key": "error",
-                            "type": "bool",
-                            "value": "true"
-                        }
-                    ]
+    spans = data['data'][0]['spans']
+    processes = data['data'][0]['processes']
 
-    json.dump(data,open(f"../injected/{fileName}",'w+'))
+    for span in spans:
+        process_id = span['processID']
+        service_name = processes.get(process_id, {}).get('serviceName')
+        if service_name in call_graph:
+            path = climb_up_spans(span, spans)
+            for path_span in path:
+                tags = path_span.get('tags', [])
+                if len(tags) == 0:
+                    path_span['tags'] = path_span.get('tags', []) + [
+                    {
+                        "key": "error",
+                        "type": "bool",
+                        "value": "true"
+                    }
+                ]
+        
+       
+
+    json.dump(data, open(f"../injected/{fileName}", 'w+'))
+
+
+
+   
